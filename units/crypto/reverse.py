db1338621426ce8aaf0d4ad234a5b084436a37f7
John Hammond <johnhammond010@gmail.com>
2019-04-29 19:39:30 -0400

    Corrected crypto.reverse to just use target.raw rather than stream

--- units/crypto/reverse.py
+++ units/crypto/reverse.py
@@ -1,7 +1,7 @@
 from unit import BaseUnit
 from collections import Counter
 import sys
-import io
+from io import StringIO
 import argparse
 from pwn import *
 import os
@@ -10,6 +10,7 @@ import units.crypto
 import string
 import collections
 
+
 # class Unit(units.PrintableDataUnit):
 class Unit(units.NotEnglishUnit):
 
@@ -17,11 +18,10 @@ class Unit(units.NotEnglishUnit):
 
 	def evaluate(self, katana, case):
 
-		with io.TextIOWrapper(self.target.stream, encoding='utf-8') as stream:
-			result = stream.read()[::-1]
-			katana.locate_flags(self, result)
-			katana.recurse(self, result)
-			katana.add_results(self, result)
+		result = self.target.raw[::-1]
+		katana.locate_flags(self, result)
+		katana.recurse(self, result)
+		katana.add_results(self, result)
 
 	
 
