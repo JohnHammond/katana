ea76063f897932adfe0582a29e7d9c238026631e
John Hammond <johnhammond010@gmail.com>
2019-04-29 19:56:44 -0400

    Brought rot47 to the new framework

---
+++ units/crypto/rot47.py
@@ -0,0 +1,35 @@
+from unit import BaseUnit
+from collections import Counter
+import sys
+from io import StringIO
+import argparse
+from pwn import *
+import os
+import units.crypto
+
+import string
+import collections
+
+
+# class Unit(units.PrintableDataUnit):
+class Unit(units.NotEnglishUnit):
+
+	PROTECTED_RECURSE = True
+
+	# Shamelessly stolen from https://rot47.net/_py/rot47.txt
+	def do_rot47(self, s):
+		x = []
+		for i in range(len(s)):
+			j = ord(s[i])
+			if j >= 33 and j <= 126:
+				x.append(chr(33 + ((j + 14) % 94)))
+			else:
+				x.append(s[i])
+		return ''.join(x)
+
+	def evaluate(self, katana, case):
+
+		content = self.do_rot47(self.target.raw)
+		katana.recurse(self,content)
+		katana.locate_flags(self, content)
+		katana.add_results(self, content)
