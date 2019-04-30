bf0f88cce7149c057d55b5c7c45e81b545aff0ab
John Hammond <johnhammond010@gmail.com>
2019-04-30 19:25:47 -0400

    Uhhhhh theoretically brought web.logon_cookies to the new framework... don't really have a valid thing to test it on...????

--- units/web/logon_cookies.py
+++ units/web/logon_cookies.py
@@ -14,9 +14,6 @@ import magic
 import units
 
 
-
-
-
 class Unit(web.WebUnit):
 
 	def __init__(self, katana, parent, target):
@@ -24,22 +21,19 @@ class Unit(web.WebUnit):
 		# Run the parent constructor, to ensure this is a valid URL
 		super(Unit, self).__init__(katana, parent, target)
 		if not katana.config['flag_format']:
-			raise units.NotApplicable
+			raise units.NotApplicable('no flag format supplied')
 
-		try:
-			self.response = requests.get(self.target)
-		except requests.exceptions.ConnectionError:
-			raise units.NotApplicable
+		raw_content = self.target.content.decode('utf-8')
 
-		self.action = re.findall(r"<\s*form.*action\s*=\s*['\"](.+?)['\"]", self.response.text, flags=re.IGNORECASE)
-		self.method = re.findall(r"<\s*form.*method\s*=\s*['\"](.+?)['\"]", self.response.text, flags=re.IGNORECASE)
+		self.action = re.findall(r"<\s*form.*action\s*=\s*['\"](.+?)['\"]", raw_content, flags=re.IGNORECASE)
+		self.method = re.findall(r"<\s*form.*method\s*=\s*['\"](.+?)['\"]", raw_content, flags=re.IGNORECASE)
 
-		self.username = re.findall(user_regex, self.response.text, flags=re.IGNORECASE)
-		self.password = re.findall(pass_regex, self.response.text, flags=re.IGNORECASE)
-		
-		# Only run this if we have potential information...
+		self.username = re.findall(web.user_regex, raw_content, flags=re.IGNORECASE)
+		self.password = re.findall(web.pass_regex, raw_content, flags=re.IGNORECASE)
+	
+			# Only run this if we have potential information...
 		if not (self.action and self.method and self.username and self.password):
-			raise NotApplicable
+			raise units.NotApplicable('no form found')
 
 	def evaluate(self, katana, case):
 		
@@ -51,7 +45,7 @@ class Unit(web.WebUnit):
 
 			s = requests.Session()
 			try:
-				r = s.request(method.lower(), self.target.rstrip('/') + '/' + action, data = { username: "anything", password : "anything"})
+				r = s.request(method.lower(), self.target.url_root.rstrip('/') + '/' + action, data = { username: "anything", password : "anything"})
 			except:
 				# JOHN: Theoretically, if we find a valid method, this should not error...
 				#       But if it does, we should see it.
