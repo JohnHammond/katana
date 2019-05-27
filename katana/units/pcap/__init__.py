d91b9c59b8592426a64eef230a7ea7e152a92c9c
John Hammond <johnhammond010@gmail.com>
2019-05-27 14:20:20 -0400

    Added some file checking protections on tcpflow and apktool which were running when they shouldn't

--- katana/units/pcap/__init__.py
+++ katana/units/pcap/__init__.py
@@ -3,14 +3,3 @@
 # @Date:   2019-02-28 22:33:18
 # @Last Modified by:   John Hammond
 # @Last Modified time: 2019-05-24 22:43:31
-
-from pwn import *
-from katana.unit import BaseUnit
-from katana import units
-import os
-
-class PcapUnit(units.FileUnit):
-	
-	def __init__(self, katana, target):
-		# This ensures that it is a file
-		super(PcapUnit, self).__init__(katana, target, keywords = 'pcap')
