fac53684412374d93b545b53bfd49db8fc28bc55
John Hammond <johnhammond010@gmail.com>
2019-08-27 19:01:02 -0400

    Added CONTRIBUTING, LICENSE, COPYING

---
+++ CONTRIBUTING.md
@@ -0,0 +1,39 @@
+Contributing
+============
+
+If you have not contributed to a project before, don't fret! GitHub has an
+[awesome guide](https://guides.github.com/activities/forking/) for helping 
+support open-source projects. Basically the steps are:
+
+1. [Fork the repository](https://guides.github.com/activities/forking/#fork)
+2. [Clone your fork](https://guides.github.com/activities/forking/#clone)
+3. [Make and push your changes](https://guides.github.com/activities/forking/#making-changes)
+4. [Make a Pull Request](https://guides.github.com/activities/forking/#making-a-pull-request)
+5. [And you're done!](https://guides.github.com/activities/forking/#huzzah)
+
+Documentation
+-------------
+
+If you do add code to Katana, __we require documentation.__ We use simple 
+[reStructuredText](http://docutils.sourceforge.net/rst.html) to produce some
+helpful ReadTheDocs pages. Please use [Google Style Python Docstrings](https://sphinxcontrib-napoleon.readthedocs.org/en/latest/example_google.html#example-google) in your code, and generally things will
+look good!
+
+Testing
+--------
+
+Admittedly we do not have formal unit tests for Katana. We **strongly** 
+encourage you to test any code that you produce with some CTF challenges.
+
+Once you are ready to do a pull-request, you should figure out if your changes
+constitutes a new feature or a bugfix in stable or beta. If it is a bugfix in
+stable or beta, you should do the pull-request against the branch in question,
+and otherwise your pull-request should be against the dev branch.
+
+Thank You!
+----------
+
+Lastly, _thank you_. This project has been a labor of love, and too often we 
+simply do not have the time to continue to maintain and add to the project. 
+We are grateful for everything little thing you do, from correcting our 
+accidentals typos to adding new units and functionality.
--- katana/units/crypto/quipqiup.py
+++ katana/units/crypto/quipqiup.py
@@ -1,12 +1,33 @@
-import json
+'''
+Substituion cipher solver, by outsourcing to https://quipqiup.com/.
+
+The gist of this code is ripped from 
+https://github.com/rallip/substituteBreaker. The unit takes the target, and
+if it does not look English text but it is clearly printable characters, it
+offers it to quipqiup online. 
+
+
+.. note::
+
+    This unit **does not recurse**. It simply looks for flags in the output of
+    quipqiup's best potential solution. Note that Katana might find flags
+    that are not in the specific flag format, but also denoted in a 
+    "the flag is:" structure.
+
+'''
+
+import json # json is used for communicating with quipqiup.com
 
 import requests
 from katana import units
 
 
-# JOHN: This is stolen from https://github.com/rallip/substituteBreaker
-#       All it does is pass the ciphertext to quipiqiup.com
-def decodeSubstitute(cipher, time=3, spaces=True):
+def decodeSubstitute(cipher:str, time=3, spaces=True) -> str:
+    '''
+    This is stolen from https://github.com/rallip/substituteBreaker
+    All it does is use the ``requests`` module to send the ciphertext to
+    quipqiup and returns the results as a string.
+    '''
     url = "https://6n9n93nlr5.execute-api.us-east-1.amazonaws.com/prod/solve"
     clues = ''
     data = {
@@ -22,24 +43,27 @@ def decodeSubstitute(cipher, time=3, spaces=True):
     return requests.post(url, data=json.dumps(data), headers=headers).text
 
 
-# class Unit(units.PrintableDataUnit):
 class Unit(units.NotEnglishAndPrintableUnit):
+    '''
+
+    '''
+
     PROTECTED_RECURSE = True
     PRIORITY = 60
 
-    def __init__(self, katana, target, keywords=None):
+    def __init__(self, katana, target):
         super(Unit, self).__init__(katana, target)
 
-        if keywords is None:
-            keywords = []
         try:
             self.raw_target = self.target.stream.read().decode('utf-8')
         except UnicodeDecodeError:
             raise units.NotApplicable("unicode error, unlikely usable cryptogram")
+
         try:
             requests.get('https://6n9n93nlr5.execute-api.us-east-1.amazonaws.com/prod/solve')
         except requests.exceptions.ConnectionError:
             raise units.NotApplicable("cannot reach quipqiup solver")
+
 
     def evaluate(self, katana, case):
 
@@ -52,8 +76,5 @@ class Unit(units.NotEnglishAndPrintableUnit):
                 found_solution = sol['plaintext']
                 best_score = sol['logp']
 
-        # katana.recurse(self, found_solution)
-        if katana.locate_flags(self, found_solution):
-            pass
-
+        katana.locate_flags(self, found_solution)
         katana.add_results(self, found_solution)
