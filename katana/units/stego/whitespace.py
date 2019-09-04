5c4288a5e6f18819460a08d5192f5d84acccada4
John Hammond <johnhammond010@gmail.com>
2019-09-04 15:01:13 -0400

    Removed the .decode() instructions so we would not have any binascii.Error errors in whitespace.py

--- katana/units/stego/whitespace.py
+++ katana/units/stego/whitespace.py
@@ -4,72 +4,63 @@ import re
 from katana import units
 from katana.units import NotApplicable
 
-
-# def encode_to_whitespace(f):
-# 	h = binascii.hexlify(f)
-# 	d = int(h, 16)
-# 	b = bin(d)[2:]
-# 	m = b.replace('0', ' ')
-# 	m = m.replace('1', '\t')
-# 	return m
-
 def decode_from_whitespace(binary_sequence):
-    decoded = int(binary_sequence, 2)
-    decoded = hex(decoded)[2:].replace('L', '')
-    try:
-        decoded = binascii.unhexlify(decoded).decode('utf-8')
-    except binascii.Error:
-        decoded = binascii.unhexlify('0' + decoded).decode('utf-8')
-    except UnicodeDecodeError:
-        return None
+	decoded = int(binary_sequence, 2)
+	decoded = hex(decoded)[2:].replace('L', '')
+	try:
+		decoded = binascii.unhexlify(decoded)
+	except binascii.Error:
+		decoded = binascii.unhexlify('0' + decoded)
+	except UnicodeDecodeError:
+		return None
 
-    return decoded
+	return decoded
 
 
 class Unit(units.FileUnit):
-    PRIORITY = 30
+	PRIORITY = 30
 
-    def __init__(self, katana, target, keywords=None):
-        super(Unit, self).__init__(katana, target)
+	def __init__(self, katana, target, keywords=None):
+		super(Unit, self).__init__(katana, target)
 
-        if keywords is None:
-            keywords = []
-        if target.is_url:
-            raise NotApplicable('target is a URL')
+		if keywords is None:
+			keywords = []
+		if target.is_url:
+			raise NotApplicable('target is a URL')
 
-        try:
-            self.space_pieces = re.findall('[ \t]+', self.target.stream.read().decode('utf-8'))
-        except (UnicodeDecodeError, AttributeError):
-            raise NotApplicable("unable to decode, might be binary")
+		try:
+			self.space_pieces = re.findall('[ \t]+', self.target.stream.read().decode('utf-8'))
+		except (UnicodeDecodeError, AttributeError):
+			raise NotApplicable("unable to decode, might be binary")
 
-        if self.space_pieces is None or self.space_pieces == []:
-            raise NotApplicable("no spaces found")
+		if self.space_pieces is None or self.space_pieces == []:
+			raise NotApplicable("no spaces found")
 
-    def evaluate(self, katana, case):
+	def evaluate(self, katana, case):
 
-        # First, retrieve all the spaces and put them together in a binary sequence.
-        pieces = []
-        for piece in self.space_pieces:
-            string_piece = piece
-            binary = string_piece.replace('\t', '1')
-            binary = binary.replace(' ', '0')
+		# First, retrieve all the spaces and put them together in a binary sequence.
+		pieces = []
+		for piece in self.space_pieces:
+			string_piece = piece
+			binary = string_piece.replace('\t', '1')
+			binary = binary.replace(' ', '0')
 
-            pieces.append(binary)
+			pieces.append(binary)
 
-        # Decode the first method, and recurse/add_results on those...
-        final_binary = ''.join(pieces)
-        decoded = decode_from_whitespace(final_binary)
+		# Decode the first method, and recurse/add_results on those...
+		final_binary = ''.join(pieces)
+		decoded = decode_from_whitespace(final_binary)
 
-        if decoded:
-            katana.add_results(self, decoded)
-            katana.recurse(self, decoded)
+		if decoded:
+			katana.add_results(self, decoded)
+			katana.recurse(self, decoded)
 
-        # Then try again, hotswapping the 1's and 0's.
-        final_binary.replace('0', "@")
-        final_binary.replace('1', "0")
-        final_binary.replace('@', "1")
+		# Then try again, hotswapping the 1's and 0's.
+		final_binary.replace('0', "@")
+		final_binary.replace('1', "0")
+		final_binary.replace('@', "1")
 
-        decoded = decode_from_whitespace(final_binary)
-        if decoded:
-            katana.add_results(self, decoded)
-            katana.recurse(self, decoded)
+		decoded = decode_from_whitespace(final_binary)
+		if decoded:
+			katana.add_results(self, decoded)
+			katana.recurse(self, decoded)
