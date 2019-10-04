b4ec596a15bcf6fde6bd2648fcb321ed28ce03a8
Caleb Stewart <caleb.stewart94@gmail.com>
2019-10-04 11:34:03 -0400

    Restricted Web.Spider unit to ignore Content-Disposition: attachment and Content-Type: application/octet-stream

--- katana/units/web/spider.py
+++ katana/units/web/spider.py
@@ -6,7 +6,7 @@ from katana.units import NotApplicable
 from hashlib import md5
 
 bad_starting_links = [
-	'#', 'javascript:', 'https://', 'http://', '//'
+	b'#', b'javascript:', b'https://', b'http://', b'//'
 ]
 
 def has_a_bad_start(link):
@@ -25,9 +25,27 @@ class Unit(WebUnit):
 	# and start to "keep track" of links we find in one specific website target
 	PROTECTED_RECURSE = True
 
+	BAD_MIME_TYPES = [ 'application/octet-stream' ]
+
+	def __init__(self, katana, target):
+		super(Unit, self).__init__(katana, target)
+
+		if 'Content-Disposition' in self.target.request.headers:
+			if 'attachment' in self.target.request.headers['Content-Disposition']:
+				raise NotApplicable('spider cannot handle attachments')
+
+		if 'Content-Type' in self.target.request.headers:
+			content_type = self.target.request.headers['Content-Type'].lower()
+			for bad_type in self.BAD_MIME_TYPES:
+				if bad_type in content_type:
+					raise NotApplicable(
+							'spider does not support {0} files'.format(bad_type)
+						)
+
 	def evaluate(self, katana, url):
 
-		links = re.findall(r'href=[\'"](.+?)[\'"]', self.target.content.decode('utf-8'), flags = re.IGNORECASE)
+		links = re.findall(rb'href=[\'"](.+?)[\'"]',
+				self.target.content, flags = re.IGNORECASE)
 		
 		# Remove anything that might not be local
 		links = list(filter(has_a_bad_start, links))
@@ -39,7 +57,7 @@ class Unit(WebUnit):
 			last_location = self.target.upstream.decode('utf-8').rstrip('/')
 
 		for link in links:
-			new_link = '{0}/{1}'.format(last_location, link.lstrip('/'))
+			new_link = '{0}/{1}'.format(last_location, link.decode('utf-8').lstrip('/'))
 			
 			# All this does is find is new links. 
 			# It won't contain flags and they don't need to be considered results.
