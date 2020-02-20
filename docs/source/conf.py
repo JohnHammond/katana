0e0dbfb95f6ac50688c05ea17c46696df379a597
Caleb Stewart <caleb.stewart94@gmail.com>
2020-02-19 20:27:40 -0500

    Fixed documentation generation so it works without installing katana

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+# Add the katana directory so imports work without installing katana w/ pip
+sys.path.insert(0, os.path.abspath("../../"))
 
 
 # -- Project information -----------------------------------------------------
